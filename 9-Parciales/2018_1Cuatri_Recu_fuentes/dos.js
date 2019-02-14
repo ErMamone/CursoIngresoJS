749648ee3d542fbc9b2c7ca0e62be20be18726f3
s3baRr00 <sebastianrilo@gmail.com>
2019-02-14 19:32:53 -0300

    Ejercicio 1 y 2 con nota conceptual

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/dos.js
@@ -1,4 +1,22 @@
 function mostrar()
 {
-  
+	var compra;
+	var compraDescuento;
+	var compraTotal;
+	var iva;
+	var descuento;
+
+	compra = prompt("Ingrese la compra:","2000");
+
+	compra = parseInt(compra);
+
+	descuento = 10/100;
+
+	compraDescuento= (compra*descuento)+compra;
+
+	iva = 21/100
+
+	compraTotal = (compraDescuento*iva) + compraDescuento;
+	
+	alert("Tu compra es de $"+compra+" tenes un descuento del 10%, queda en: "+compraDescuento+", mas el iva es de $"+compraTotal);
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,22 @@
 
 function mostrar()
 {
+	var mascota1;
+	var mascota2;
+	var peso1;
+	var peso2;
+	var suma;
 
+	mascota1 = prompt("Ingrese el nombre de su mascota: ", "");
+	mascota2 = prompt("Ingrese el nombre de su segunda mascota: ", "");
+
+	peso1 = prompt("Ingrese el peso de la primera mascota: ","");
+	peso2 = prompt("Ingrese el peso de la segunda mascota: ","");
+
+	peso1= parseInt(peso1);
+	peso2= parseInt(peso2);
+
+	suma = peso1+peso2;
+
+	alert("Tenes dos mascotas"+mascota1+" y "+mascota2+ ", que pesan "+peso1+" y "+peso2+" kg, la suma de estos es de: "+suma);
 }
