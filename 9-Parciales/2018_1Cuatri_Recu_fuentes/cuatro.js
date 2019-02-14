391ef9919b0480a913b82f2bc6e9a0d05d5ff83c
s3baRr00 <sebastianrilo@gmail.com>
2019-02-14 19:58:23 -0300

    Ejercicio 4 conceptual

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -6,10 +6,15 @@ function mostrar()
 	var precioTotal;
 	var iva;
 	var propina;
+	var cantPersonas;
+	var precioDividido;
 
 	precio = prompt("Precio a pagar:","1000");
 
+	cantPersonas = prompt("Cantidad de personas que pagan", "5");
+
 	precio = parseInt(precio);
+	cantPersonas= parseInt(cantPersonas);
 
 	iva= 21/100;
 	precioIva= precio*iva;
@@ -17,10 +22,13 @@ function mostrar()
 	propina= 10/100;
 	precioPropina= precio*propina;
 
-	precioTotal=precioPropina+precioIva+precio
+	precioTotal=precioPropina+precioIva+precio;
+
+	precioDividido= precioTotal/cantPersonas;
 
 
-	alert("El precio es: "+precio+", el iva es "+precioIva+", la propina es del 10%: "+precioPropina+" y el precio total es de: "+precioTotal)
+
+	alert("El precio es: "+precio+", el iva es "+precioIva+", la propina es del 10%: "+precioPropina+" y el precio total es de: "+precioTotal+" Cada uno debe pagar: "+precioDividido);
 }
 //en un restuariante un grupo de amigos quiere saber cuanto debe pagar cada uno del total, 
 //recordar que se debe agregar el 10% de propina, y que los precios no incluyen el iva(21%)
