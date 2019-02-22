689710a89b97850eb5fbe796639235b828a3bb50
alumno <alumno>
2019-02-22 19:26:26 -0300

    While terminado

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -1,17 +1,58 @@
 function mostrar()
 {
-
-	var contador=0;
-	//declarar contadores y variables 
-	
 	var respuesta="si";
+	var num;
+	var contador=0;
+	var positivo=0;
+	var negativo=0;
+	var respuesta='si';
+	var contadorPositivos;
+	var contadorNegativos;
+	var contadorCeros;
+	var contadorPar;
+	var contadorImpar;
+	var promedioPositivos;
+	var promedioNegativos;
+	var promedioTotal;
 
-	while(respuesta!="no")
-	{
-		
-	
+	contadorPositivos=0;
+	contadorNegativos=0;
+	contadorCeros=0;
+	contadorImpar=0;
+	contadorPar=0;
+
+	while(respuesta=="si"){
+		num=prompt("ingrese un numero");
+		num=parseInt(num);
+		if (num>0) {
+			positivo=positivo+num;
+			contadorPositivos=contadorPositivos+1;
+		}else{
+			negativo=negativo+num;
+			contadorNegativos=contadorNegativos+1;
+		}
+		if (num==0) {
+			contadorCeros=contadorCeros+1;
+		}
+		if (num/2) {
+			contadorPar=contadorPar+1;
+		}else{
+			contadorImpar=contadorImpar+1;
+		}
+		contador++;
+		respuesta=prompt("Desea ingresar mas numeros?");
 	}
 
+	promedioPositivos=positivo/contadorPositivos;
+	promedioNegativos=negativo/contadorNegativos;
+	promedioTotal=positivo+negativo;
+
+alert("La cantidad de positivos es: "+	contadorPositivos+
+	" -- el promedio de positivos es: "+	promedioPositivos+
+	" -- La cantidad de negativos es: "+	contadorNegativos+
+	" -- el promedio de negativos es: "+	promedioNegativos+
+	" -- La cantidad de 0 es de: "+	contadorCeros+
+	" -- y la diferencia entre positivos y negativos es: "+promedioTotal)
 
 
 
--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -1,17 +1,36 @@
 function mostrar()
 {
 
-	var contador=0;
+	var contador;
+	var numMayor;
+	var numMenor;
+	var num;
+	var respuesta;
 	// declarar variables
 	
-	var respuesta='si';
+	respuesta='si';
+	contador=0;
+	numMayor=0;
+	numMenor=9999999999;
 
-	while(respuesta!='no')
+	while(respuesta=='si')
 	{
-		
-	
+		num=prompt("ingrese un numero");
+		num=parseInt(num);
+		if (numMayor<=num) {
+			numMayor=num;
+		}
+		if (num<numMenor) {
+			numMenor=num;
+		}
+		contador++;
+		respuesta=prompt("Desea ingresar mas numeros?");
 	}
-
+	numMayor=parseInt(numMayor);
+	numMenor=parseInt(numMenor);
+	maximo.value=numMayor;
+	minimo.value=numMenor;
+	alert("La cantidad de numero agregados son: "+contador);
 
 
 
