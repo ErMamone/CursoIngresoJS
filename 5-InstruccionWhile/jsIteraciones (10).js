d0b492521c9a360fc3110a3df22ef439935d7c1d
alumno <alumno>
2019-02-22 22:04:37 -0300

    recuperatorio 7

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -9,56 +9,61 @@ function mostrar()
 	var contadorNegativos;
 	var contadorCeros;
 	var contadorPar;
-	var contadorImpar;
 	var promedioPositivos;
 	var promedioNegativos;
 	var promedioTotal;
 
-	respuesta="si";
+	respuesta=true;
 	contador=0;
 	positivo=0;
 	negativo=0;
 	contadorPositivos=0;
 	contadorNegativos=0;
 	contadorCeros=0;
-	contadorImpar=0;
 	contadorPar=0;
 	promedioNegativos=0;
 	promedioPositivos=0;
 
-	while(respuesta=="si"){
+	while(respuesta==true)
+	{
 		num=prompt("ingrese un numero");
 		num=parseInt(num);
-		if (num>0) {
+
+		if (num>0) 
+		{
 			positivo=positivo+num;
 			contadorPositivos=contadorPositivos+1;
-		}else{
-			negativo=negativo+num;
-			contadorNegativos=contadorNegativos+1;
-		}
-		if (num%10 == 0) {
-			contadorCeros=contadorCeros+1;
-		}
-		if (num%2==0) {
+		}else
+			{if (num<0) 
+				{
+				negativo=negativo+num;
+				contadorNegativos=contadorNegativos+1;
+				}
+				else{
+					contadorCeros=contadorCeros+1;
+				}
+			}
+		if (num%2==0) 
+		{
 			contadorPar=contadorPar+1;
-		}else{
-			contadorImpar=contadorImpar+1;
 		}
+
 		contador++;
-		respuesta=prompt("Desea ingresar mas numeros?");
+		respuesta=confirm("Presiona 'Aceptar' o la tecla 'Enter' para ingresar otro numero, sino presione cancelar");
 	}
 
 	promedioPositivos=positivo/contadorPositivos;
 	promedioNegativos=negativo/contadorNegativos;
 	promedioTotal=positivo+negativo;
 
-alert("La cantidad de positivos es: "+contadorPositivos+
-	" -- El promedio de positivos es: "+promedioPositivos+
-	" -- La cantidad de negativos es: "+contadorNegativos+
-	" -- El promedio de negativos es: "+promedioNegativos+
-	" -- La cantidad de ceros es de: "+	contadorCeros+
-	" -- La cantidad de numeros pares es: "+contadorPar+
-	" -- La cantidad de numero impares: "+contadorImpar+
-	" -- La diferencia entre positivos y negativos es: "+promedioTotal);
+	document.write("//////_1) La suma de Negativos es: "+negativo);
+	document.write("<br>//////_2) La suma de positivos es: "+positivo);
+	document.write("<br>//////_3) La cantidad de positivos es: "+contadorPositivos);
+	document.write("<br>//////_4) La cantidad de negativos es: "+contadorNegativos);
+	document.write("<br>//////_5) La cantidad de ceros es de: "+contadorCeros);
+	document.write("<br>//////_6) La cantidad de numeros pares es: "+contadorPar);
+	document.write("<br>//////_7) El promedio de positivos es: "+promedioPositivos);
+	document.write("<br>//////_8) El promedio de negativos es: "+promedioNegativos);
+	document.write("<br>//////_9) La diferencia entre positivos y negativos es: "+promedioTotal);
 
 }//FIN DE LA FUNCIÓN
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,4 +1,53 @@
 function mostrar()
-{
+{	
+	var alumnos;
+	var sexo;
+	var nota;
+	var notaTotales;
+	var notaBaja;
+	var	sexoBaja;
+	var cantV;
+	var promedio;
+
+	alumnos=0;
+	notaTotales=0;
+	notaBaja=1;
+	cantV=0;
+
+	while(alumnos<5)
+	{
+		sexo=prompt("Ingrese su sexo");
+
+		while(sexo!='f' && sexo!='m'){
+			sexo=prompt("Error, porfavor ingrese su sexo: ");
+		}
+		
+	
+		nota=prompt("Ingrese su nota(entre 1 y 10): ","");
+		nota=parseInt(nota);
+
+		
+		while(nota!=0 && nota>10){
+			nota=prompt("Error, ingrese su nota(entre 1 y 10): ","");
+			nota=parseInt(nota);
+		}
+		
+
+		notaTotales=notaTotales+nota;
+
+		if (nota<notaBaja) {
+			notaBaja=nota;
+			sexoBaja=sexo;
+		}
+
+		if (sexo="m" && nota>=6) {
+			cantV=cantV+1;
+		}
+		alumnos++;
+	}
+
+	promedio=notaTotales/5;
+	alert("El promedio de notas totales es: "+promedio+"   La nota mas baja: "+notaBaja+" y el sexo es: "+sexoBaja+
+		"   La cantidad de varones con mas de 6 es: "+cantV);
 
 }
